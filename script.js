349f3842802b180c3643391550a651fefe2e2160
Zachary Clark <zacclark369@gmail.com>
2022-08-17 15:00:34 -0700

    added time formating and element call

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
 const newYears = '1 Jan 2023';
 
 function countdown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const seconds = (newYearsDate - currentDate) / 1000;
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-    const minutes = Math.floor(seconds / 60);
+    const minutes = Math.floor(totalSeconds / 60) % 60;
 
-    const hours = Math.floor(minutes / 60);
-    const days = Math.floor(seconds / 3600 / 24);
+    const hours = Math.floor(minutes / 60) % 24;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    
+    const seconds = Math.floor(totalSeconds) % 60;
 
-    console.log(days);
+    daysEl.innerHTML =days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
 }
 
 // initial call
